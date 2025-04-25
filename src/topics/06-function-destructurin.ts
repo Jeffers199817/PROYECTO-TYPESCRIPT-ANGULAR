7670aa6015221368807a9ca6d026e9a8708e562c
Jeffers1998 <jefferson.alquinga1998@gmail.com>
2025-04-24 21:24:20 -0500

    Integración de typescript

--- src/topics/06-function-destructurin.ts
+++ src/topics/06-function-destructurin.ts
@@ -24,6 +24,7 @@ interface TaxCalculationOptions{
 function taxCalculation(options: TaxCalculationOptions): [number,number]{
     const { tax, products } = options;
     let total = 0;
+    
 
     products.forEach(({price}) => {
         total += price;
