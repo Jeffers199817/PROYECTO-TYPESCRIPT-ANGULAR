7494c153c077e09db152986318cfbad4cc0cd68f
Jeffers1998 <jefferson.alquinga1998@gmail.com>
2025-04-29 13:05:28 -0500

    Integración de decoradores en typescript

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,8 @@ import './style.css';
 //import './topics/06-function-destructurin';
 //import './topics/07-import-export';
 //import './topics/08-clases';
-import './topics/09-generics';
+//import './topics/09-generics';
+import './topics/10.-decorators';
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `Hola Mundo`;
